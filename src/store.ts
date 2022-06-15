19408fe5708fffc24474de33bae41343ef97e807
Simon Piquet <piquet.simon@yahoo.fr>
2022-06-15 21:37:48 +0200

    (greenly-test-typescript):score.ts first refactoring

--- src/store.ts
+++ src/store.ts
@@ -1,61 +1,74 @@
-import { DiscountOffer } from "./discountOffer";
+import { DiscountOffer } from "./DiscountOffer";
+
+export const DiscountOffers = new Map([
+  [
+    "Naturalia",
+    (expiresIn: number, discountInPercent: number) => {
+      expiresIn = expiresIn - 1;
+      if (discountInPercent < 50) {
+        expiresIn < 0 ? (discountInPercent += 2) : (discountInPercent += 1);
+      }
+      return { expiresIn, discountInPercent };
+    }
+  ],
+  [
+    "Ilek",
+    (expiresIn: number, discountInPercent: number) => ({
+      expiresIn,
+      discountInPercent
+    })
+  ],
+  [
+    "Vinted",
+    (expiresIn: number, discountInPercent: number) => {
+      expiresIn = expiresIn - 1;
+      if (discountInPercent < 50) {
+        discountInPercent = discountInPercent + 1;
+        expiresIn < 11 && (discountInPercent += 1);
+        expiresIn < 6 && (discountInPercent += 1);
+        expiresIn < 0 && (discountInPercent = 0);
+      }
+      return { expiresIn, discountInPercent };
+    }
+  ]
+]);
 
 export class Store {
   public discountOffers: DiscountOffer[];
-  constructor(discountOffers = []) {
+
+  constructor(discountOffers: DiscountOffer[] = []) {
     this.discountOffers = discountOffers;
   }
-  updateDiscounts() {
-    for (var i = 0; i < this.discountOffers.length; i++) {
-      if (
-        this.discountOffers[i].partnerName != "Naturalia" &&
-        this.discountOffers[i].partnerName != "Vinted"
-      ) {
-        if (this.discountOffers[i].discountInPercent > 0) {
-          if (this.discountOffers[i].partnerName != "Ilek") {
-            this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent - 1;
-          }
-        }
-      } else {
-        if (this.discountOffers[i].discountInPercent < 50) {
-          this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent + 1;
-          if (this.discountOffers[i].partnerName == "Vinted") {
-            if (this.discountOffers[i].expiresIn < 11) {
-              if (this.discountOffers[i].discountInPercent < 50) {
-                this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent + 1;
-              }
-            }
-            if (this.discountOffers[i].expiresIn < 6) {
-              if (this.discountOffers[i].discountInPercent < 50) {
-                this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.discountOffers[i].partnerName != "Ilek") {
-        this.discountOffers[i].expiresIn = this.discountOffers[i].expiresIn - 1;
-      }
-      if (this.discountOffers[i].expiresIn < 0) {
-        if (this.discountOffers[i].partnerName != "Naturalia") {
-          if (this.discountOffers[i].partnerName != "Vinted") {
-            if (this.discountOffers[i].discountInPercent > 0) {
-              if (this.discountOffers[i].partnerName != "Ilek") {
-                this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent - 1;
-              }
-            }
-          } else {
-            this.discountOffers[i].discountInPercent =
-              this.discountOffers[i].discountInPercent - this.discountOffers[i].discountInPercent;
-          }
-        } else {
-          if (this.discountOffers[i].discountInPercent < 50) {
-            this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent + 1;
-          }
-        }
-      }
-    }
 
-    return this.discountOffers;
+  defaultComputeDiscount(expiresIn: number, discountInPercent: number) {
+    expiresIn = expiresIn - 1;
+    if (discountInPercent > 0) {
+      expiresIn < 0 ? (discountInPercent -= 2) : (discountInPercent -= 1);
+    }
+    return { expiresIn, discountInPercent };
+  }
+
+  getComputeDiscountMethod(partnerName: string) {
+    if (DiscountOffers.has(partnerName)) {
+      return DiscountOffers.get(partnerName);
+    }
+    return this.defaultComputeDiscount;
+  }
+
+  updateDiscounts(): DiscountOffer[] {
+    return this.discountOffers = this.discountOffers.map(discountOffer => {
+      let discountComputingFunction = this.getComputeDiscountMethod(
+        discountOffer.partnerName
+      );
+      const { expiresIn, discountInPercent } = discountComputingFunction(
+        discountOffer.expiresIn,
+        discountOffer.discountInPercent
+      );
+      return {
+        ...discountOffer,
+        expiresIn,
+        discountInPercent
+      };
+    });
   }
 }
